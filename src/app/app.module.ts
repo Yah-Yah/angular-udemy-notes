87c39994d4f8aa7e08bc25a0948f26897f54bf64
Janina Zylinska <janeczka@gmail.com>
2020-07-11 20:07:05 +0100

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// STEP_04 in order for that ngModel inside input to work we need to import FormsModule
-// this part is typescript
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    // this part is angular
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
